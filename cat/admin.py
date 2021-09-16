c82687ac2464a90896b49bb800c09d7e20039d53
aleksey1652 <aleksey1652@ukr.net>
2021-09-16 19:23:15 +0300

    34 version of application moved into github

---
+++ cat/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from .models import *
+
+
+admin.site.register(Parts_full)
+admin.site.register(Articles)
+admin.site.register(Providers)
+admin.site.register(Pc_assembly)
+admin.site.register(Computers)
+admin.site.register(Cooler)
+admin.site.register(CPU)
+admin.site.register(MB)
+admin.site.register(RAM)
+admin.site.register(GPU)
+admin.site.register(PSU)
+admin.site.register(HDD)
+admin.site.register(SSD)
+admin.site.register(FAN)
+admin.site.register(CASE)
